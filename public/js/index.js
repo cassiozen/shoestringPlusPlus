ae893ccae366c2c92a947677d5d03a61a6c5c85a
Dan <daniel.sohval@gmail.com>
2017-08-21 13:52:29 -0400

    updating phrases

--- public/js/index.js
+++ public/js/index.js
@@ -8,8 +8,18 @@ $(() => {
     "images/VR5.jpg",
     "images/VR6.jpg"
   ];
-  let currentImageIndex = 0;
-  setInterval(() => {
+
+  const phrases = [
+    'A Revolutionary New Product',
+    'Making The World A Better Place',
+    'A Disruptive New Platform',
+    'Bleeding-Edge Tech'
+  ];
+
+  let currentImageIndex = 0
+  let currentPhraseIndex = 0;
+
+  const updateImg = () => {
     currentImageIndex++;
     if (currentImageIndex === images.length) currentImageIndex = 0;
     $image = $("#carousel-image");
@@ -17,5 +27,21 @@ $(() => {
       $image.attr("src", images[currentImageIndex]);
     });
     $image.fadeIn();
+  };
+
+  const updateText = () => {
+    currentPhraseIndex++
+    if (currentPhraseIndex=== phrases.length) currentPhraseIndex = 0;
+    $txt = $("#carousel-text");
+    $txt.fadeOut(el => {
+      $txt.text(phrases[currentPhraseIndex]);
+    });
+    $txt.fadeIn();
+  }
+
+  setInterval(() => {
+    updateImg();
+    updateText();
   }, 5000);
+
 });
