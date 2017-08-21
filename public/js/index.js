ca4faaa1114d134c1def9d4218a377ffcf52cc15
Cássio Zen <cassiozen@gmail.com>
2017-08-21 12:58:02 -0400

    Draft carousel

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,23 @@
 $(() => {
   console.log("helloooo");
+
+  // Carousel
+  const images = [
+    "images/VR1.jpg",
+    "images/VR2.jpg",
+    "images/VR3.jpg",
+    "images/VR4.jpg",
+    "images/VR5.jpg",
+    "images/VR6.jpg"
+  ];
+  let currentImageIndex = 0;
+  setInterval(() => {
+    currentImageIndex++;
+    if (currentImageIndex === images.length) currentImageIndex = 0;
+    $image = $("#carousel-image");
+    $image.fadeOut(el => {
+      $image.attr("src", images[currentImageIndex]);
+      $image.fadeIn();
+    });
+  }, 5000);
 });
